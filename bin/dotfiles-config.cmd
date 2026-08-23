097c71511ecf3b79d54a57a43f81f935a25afcaa
arakilian0 <arakilian0@gmail.com>
2026-08-22 23:38:55 -0500

    chore: remove old dotfiles

--- bin/dotfiles-config.cmd
+++
@@ -1,4 +0,0 @@
-@REM PATHS
-set dir_back=../
-set dir_dev=C:\Development
-set dir_dotfiles=%dir_dev%\Software\dotfiles
--- bin/dotfiles.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-call dotfiles-config
-IF [%~1]==[] ( cd %dir_dotfiles% ) ELSE (
-  IF "%1"=="-u" ( code %dir_dotfiles% )
-  IF "%1"=="-o" ( open %dir_dotfiles% )
-)
