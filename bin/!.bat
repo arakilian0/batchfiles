ac0493ef16fda36fbb972229c5eadeaa9f02a761
arakilian0 <arakilian0@gmail.com>
2026-08-24 06:05:29 -0500

    feat(!): force option

--- bin/!.bat
+++ bin/!.bat
@@ -1,27 +1,30 @@
 @ECHO OFF
 
-:: Callback handling
 IF /I "%~1"=="--internal-usage" GOTO usage
 
-:: 1. Delegate common flags to common_flags.bat
 CALL "%~dp0lib\common.bat" "%~1" "%~f0"
 IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 
-:: 2. Handle script-specific flags / defaults
 IF [%~1]==[] GOTO default_action
+IF /I "%~1"=="-f" GOTO exit_window
+IF /I "%~1"=="--force" GOTO exit_window
 
-:: 3. Catch invalid arguments HERE (only after checking all valid options)
 ECHO %RED%Error:%RESET% %DIM%"%~1"%RESET% is not an acceptable argument.
 GOTO usage
 
 :default_action
 EXIT
 
+:exit_window
+taskkill /F /IM wt.exe /IM OpenConsole.exe /IM WindowsTerminal.exe 2>nul
+EXIT
+
 :usage
 ECHO.
-ECHO Usage:%CYAN% %~n0 %RESET%%DIM%[.] [-h]%RESET%
+ECHO Usage:%CYAN% %~n0 %RESET%%DIM%[.] [-f] [-h]%RESET%
 ECHO.
 ECHO   %DIM%(no args)%RESET%           Quit current session
 ECHO   %DIM%.%RESET%                   Open %~nx0 in VS Code
+ECHO   %DIM%-f, --force%RESET%         Quit all terminal processes
 ECHO   %DIM%-h, --help%RESET%          Show help
 EXIT /B 1
