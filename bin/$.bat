0f832c1353df72b4211712ddf022c0a81c58c28b
arakilian0 <arakilian0@gmail.com>
2026-08-24 01:53:47 -0500

    refactor($): move new.bat logic into $.bat, conditional handler

--- bin/$.bat
+++ bin/$.bat
@@ -1,2 +1,24 @@
 @ECHO OFF
+
+IF [%~1]==[] GOTO start_terminal
+IF /I "%~1"=="-p" GOTO system_props
+IF /I "%~1"=="-h" GOTO usage
+IF /I "%~1"=="--help" GOTO usage
+
+ECHO Error: %1 is not an acceptable argument.
+GOTO usage
+
+:start_terminal
+start cmd.exe
+GOTO :EOF
+
+:system_props
 start SystemPropertiesAdvanced.exe
+GOTO :EOF
+
+:usage
+ECHO Usage: %~n0 [-p] [-h]
+ECHO   (no args)        Command prompt
+ECHO   -p               System properties
+ECHO   -h, --help       Show this help
+EXIT /B 0
