06e5ca5b704b1f3b6ede43dbecbe021229c63414
arakilian0 <arakilian0@gmail.com>
2026-08-24 02:29:33 -0500

    feat(!): error handling

--- bin/!.bat
+++ bin/!.bat
@@ -1,2 +1,24 @@
 @ECHO OFF
+
+IF [%~1]==[] GOTO exit_code
+
+IF "%~1"=="." GOTO script_code
+IF /I "%~1"=="-h" GOTO usage
+IF /I "%~1"=="--help" GOTO usage
+
+ECHO Error: %1 is not an acceptable argument.
+GOTO usage
+
+:script_code
+code "%~f0"
+EXIT /B 0
+
+:exit_code
 exit
+
+:usage
+ECHO Usage: %~n0 [.] [-h]
+ECHO   (no args)        Exit terminal
+ECHO   .                Open this script in VS Code
+ECHO   -h, --help       Show this help
+EXIT /B 0
