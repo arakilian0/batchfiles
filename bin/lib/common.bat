ff4a82699790dc374e3b8d749e721a48b42138f3
arakilian0 <arakilian0@gmail.com>
2026-08-24 03:52:59 -0500

    refactor: common > common_flags

--- bin/lib/common.bat
+++ bin/lib/common.bat
@@ -1,5 +1,22 @@
 @ECHO OFF
 
+:: Define ESC character safely
+FOR /F "tokens=1,2 delims=#" %%a IN ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') DO SET "ESC=%%b"
+
+:: Set Color Variables (ANSI codes)
+SET "RED=%ESC%[31m"
+SET "GREEN=%ESC%[32m"
+SET "YELLOW=%ESC%[33m"
+SET "BLUE=%ESC%[34m"
+SET "MAGENTA=%ESC%[35m"
+SET "CYAN=%ESC%[36m"
+SET "WHITE=%ESC%[37m"
+SET "GRAY=%ESC%[90m"
+SET "BOLD=%ESC%[1m"
+SET "DIM=%ESC%[2m"
+SET "REGULAR=%ESC%[22m"
+SET "RESET=%ESC%[0m"
+
 :: %1 = Argument passed to caller script
 :: %2 = Full path of caller script (%~f0)
 
