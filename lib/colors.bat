6eaf6ef882bd90f1da62ecc973a30063cad06b40
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-08-27 10:56:59 -0500

    refactor: copy bin files to src and lib

---
+++ lib/colors.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+FOR /F "tokens=1,2 delims=#" %%a IN ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') DO SET "ESC=%%b"
+
+SET "RED=%ESC%[31m"
+SET "GREEN=%ESC%[32m"
+SET "YELLOW=%ESC%[33m"
+SET "BLUE=%ESC%[34m"
+SET "MAGENTA=%ESC%[35m"
+SET "CYAN=%ESC%[36m"
+SET "WHITE=%ESC%[37m"
+SET "GRAY=%ESC%[90m"
+SET "BOLD=%ESC%[1m"
+SET "DIM=%ESC%[2m"
+SET "REGULAR=%ESC%[22m"
+SET "RESET=%ESC%[0m"
