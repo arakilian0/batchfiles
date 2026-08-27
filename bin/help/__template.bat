38938b4524585ae3cae2fb9eb8bd538423940fb9
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-08-27 11:01:47 -0500

    chore: remove bin/

--- bin/help/__template.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-CALL "%~dp0..\lib\colors.bat"
-
-ECHO Usage:%CYAN% %~1 %RESET%%GRAY%[-e ^| --edit] [-h ^| --help]%RESET%
-ECHO.
-ECHO   %GRAY%(no args)%RESET%           No argument handler
-ECHO   %GRAY%-e, --edit%RESET%          Open %~2 in VS Code or Notepad
-ECHO   %GRAY%-h, --help%RESET%          Show help
-EXIT /B 1
