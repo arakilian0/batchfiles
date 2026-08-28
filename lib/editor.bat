9fe75ad267d724e92bac0d801f485ca72e3c9a03
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-08-27 22:08:00 -0500

    fix(lib/editor): fallback handler

--- lib/editor.bat
+++ lib/editor.bat
@@ -3,9 +3,21 @@
 WHERE code >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 GOTO fallback
 
+IF "%~2" == "full" (
+    code "%~1"
+    EXIT /B 0
+) 
+
 code "%~1"
 EXIT /B 0
 
 :fallback
+IF "%~2" == "full" (
+    ECHO Error: Default editor VS Code not found on System PATH.
+    ECHO Configure your editor in windk/lib/editor.bat
+    EXIT /B 0
+)
+
 notepad "%~1"
 EXIT /B 0
+
