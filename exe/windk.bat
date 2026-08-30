f70d14cca30515eab7d57df891a46fdc7c9d0863
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-08-30 01:17:02 -0500

    feat: create unarchive

--- exe/windk.bat
+++ exe/windk.bat
@@ -61,6 +61,7 @@ IF "%OPEN_WINDK_IN_EDITOR%"=="1" CALL "%SCRIPT_DIR%..\lib\windk\flags.bat" :open
 :: 4. Subcommands & Directory Navigation
 IF DEFINED COMMAND (
     IF "!COMMAND!"=="archive" CALL "%SCRIPT_DIR%..\lib\windk\archive.bat" & EXIT /B 0
+    IF "!COMMAND!"=="unarchive" CALL "%SCRIPT_DIR%..\lib\windk\unarchive.bat" & EXIT /B 0
     IF "!COMMAND!"=="delete" CALL "%SCRIPT_DIR%..\lib\windk\delete.bat" & EXIT /B 0
     IF "!COMMAND!"=="create" CALL "%SCRIPT_DIR%..\lib\windk\create.bat" & EXIT /B 0
 
---
+++ lib/windk/unarchive.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+
+CALL "%~dp0..\..\util\color.bat"
+
+:: 1. Check if the delayed variable is empty
+IF "!SUBCOMMAND_ARGS!"=="" (
+    ECHO %RED%Error:%RESET% please provide the name of the script to %~n0.
+    GOTO end
+)
+
+IF EXIST "%~dp0..\..\.archive\!SUBCOMMAND_ARGS!.bat" (
+    GOTO prompt_action
+) ELSE (
+    ECHO %RED%Error:%RESET% "windk\.archive\!SUBCOMMAND_ARGS!.bat" does not exist.
+    GOTO end
+)
+
+:prompt_action
+choice /C YN /M "Are you sure you want %~n0 !SUBCOMMAND_ARGS!?"
+
+IF %ERRORLEVEL% EQU 1 (
+    move /-Y "%~dp0..\..\.archive\!SUBCOMMAND_ARGS!.bat" "%~dp0..\..\exe\!SUBCOMMAND_ARGS!.bat" >nul
+    IF EXIST "%~dp0..\..\.archive\help\!SUBCOMMAND_ARGS!-help.bat" (
+        move /-Y "%~dp0..\..\.archive\help\!SUBCOMMAND_ARGS!-help.bat" "%~dp0..\..\help\!SUBCOMMAND_ARGS!-help.bat" >nul
+    )
+    GOTO end
+) ELSE IF %ERRORLEVEL% EQU 2 (
+    ECHO Deletion aborted. No changes were made.
+    GOTO end
+)
+
+:end
+ENDLOCAL
