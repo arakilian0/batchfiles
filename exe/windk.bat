e3b2bc2262c0f5d48d9070112bea68c746e5a777
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-08-30 00:41:25 -0500

    feat: windk command delete

--- exe/windk.bat
+++ exe/windk.bat
@@ -60,7 +60,7 @@ IF "%OPEN_WINDK_IN_EDITOR%"=="1" CALL "%SCRIPT_DIR%..\lib\windk\flags.bat" :open
 
 :: 4. Subcommands & Directory Navigation
 IF DEFINED COMMAND (
-    IF "!COMMAND!"=="install" CALL "%SCRIPT_DIR%..\lib\windk\install.bat" & EXIT /B 0
+    IF "!COMMAND!"=="delete" CALL "%SCRIPT_DIR%..\lib\windk\delete.bat" & EXIT /B 0
     IF "!COMMAND!"=="create" CALL "%SCRIPT_DIR%..\lib\windk\create.bat" & EXIT /B 0
 
     :: If neither condition matched:
