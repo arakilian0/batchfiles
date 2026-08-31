32112490cb0ee757635d6badac75a262884a0366
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-08-31 03:10:00 -0500

    chore: remove color.cfg

--- lib/__utility/color.bat
+++ lib/__utility/color.bat
@@ -1,23 +1,20 @@
 @ECHO OFF
 
-:: Import Colors from cfg/windk.cfg
-CALL "%~dp0config.bat" "color"
-
 :: Generate %ESC% character
 FOR /F "tokens=1,2 delims=#" %%a IN ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') DO SET "ESC=%%b"
 
 :: Set Color Variables
-SET "RED=%ESC%[%_red%"
-SET "GREEN=%ESC%[%_green%"
-SET "YELLOW=%ESC%[%_yellow%"
-SET "BLUE=%ESC%[%_blue%"
-SET "MAGENTA=%ESC%[%_magenta%"
-SET "CYAN=%ESC%[%_cyan%"
-SET "WHITE=%ESC%[%_white%"
-SET "GRAY=%ESC%[%_gray%"
-SET "BOLD=%ESC%[%_bold%"
-SET "DIM=%ESC%[%_dim%"
-SET "REGULAR=%ESC%[%_regular%"
-SET "RESET=%ESC%[%_reset%"
+SET "RED=%ESC%[31m"
+SET "GREEN=%ESC%[32m"
+SET "YELLOW=%ESC%[33m"
+SET "BLUE=%ESC%[34m"
+SET "MAGENTA=%ESC%[35m"
+SET "CYAN=%ESC%[36m"
+SET "WHITE=%ESC%[37m"
+SET "GRAY=%ESC%[90m"
+SET "BOLD=%ESC%[1m"
+SET "DIM=%ESC%[2m"
+SET "REGULAR=%ESC%[22m"
+SET "RESET=%ESC%[0m"
 
 EXIT /B 0
