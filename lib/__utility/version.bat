0a5b6e0c6d32d89a3af3e8d0302e597d8e7b1b65
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-08-31 23:04:15 -0500

    fix: version config handler

--- lib/__utility/version.bat
+++ lib/__utility/version.bat
@@ -1,4 +1,14 @@
 @ECHO OFF
-CALL "%~dp0config.bat" "package"
-ECHO %NAME% version %VERSION%
+
+SET "config_parser=%~dp0\config.bat"
+SET "config_file=%~dp0..\..\config\windk.cfg"
+
+:: Parse configuration
+call "!config_parser!" "!config_file!"
+if !errorlevel! neq 0 (
+    echo [ERROR] Parser failed with exit code: !errorlevel!
+    exit /b !errorlevel!
+)
+
+ECHO %MAIN_NAME% version %MAIN_VERSION%
 EXIT /B 0
