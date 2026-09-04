0aaca6eea2b2582512416f4c5e61fe94a84d1328
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-09-04 05:28:58 -0500

    feat: create windk alias dk

---
+++ bin/dk.bat
@@ -0,0 +1,12 @@
+@echo off
+:: bin/windk.bat - Entry Proxy & Scope Guard
+setlocal EnableExtensions EnableDelayedExpansion
+
+:: Forward all arguments to the tool dispatcher
+call "%~dp0..\tools\windk\cli.bat" %*
+
+:: Store return code from dispatcher
+set "EXIT_CODE=%ERRORLEVEL%"
+
+:: Destroy all windk variables and restore user's previous environment
+endlocal & exit /b %EXIT_CODE%
